78b9646e4baa521ca87b474be0e31b768404198d
Christopher Guess <cguess@gmail.com>
2019-09-30 22:32:49 -0400

    initial commit

---
+++ app/src/main/java/addomanager/bfaglobal/com/push/MainActivity.kt
@@ -0,0 +1,19 @@
+package addomanager.bfaglobal.com.push
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import addomanager.bfaglobal.com.push.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
